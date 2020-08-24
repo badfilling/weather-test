add9002096a55b99c2c8ce0af8f973f530154c58
stepaniuk.artur <211650@edu.p.lodz.pl>
2020-08-24 19:52:05 +0200

    detail vc = basic data provided

--- Weather-task/Screens/LocationWeather/view/LocationWeatherViewController.swift
+++ Weather-task/Screens/LocationWeather/view/LocationWeatherViewController.swift
@@ -7,9 +7,38 @@
 //
 
 import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
 
 class LocationWeatherViewController: UIViewController {
     let viewModel: LocationWeatherViewModel
+    
+    let collectionView: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collection.backgroundColor = .none
+        return collection
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    let weatherIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 30, weight: .bold)
+        return label
+    }()
     
     init(viewModel: LocationWeatherViewModel) {
         self.viewModel = viewModel
@@ -20,9 +49,81 @@ class LocationWeatherViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
-        title = "stub for weather"
-//        self.navigationItem.back
+//        title = "stub for weather"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.isNavigationBarHidden = true
+        setupViews()
+        bindViewsRx()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.isNavigationBarHidden = false
+    }
+    func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+        }
+        
+        let rowStack = UIStackView()
+        rowStack.axis = .horizontal
+        rowStack.distribution = .equalSpacing
+        rowStack.addArrangedSubview(weatherIcon)
+        rowStack.addArrangedSubview(temperatureLabel)
+        view.addSubview(rowStack)
+        
+        rowStack.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.6)
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+        }
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(rowStack.snp.bottom).offset(16)
+        }
+    }
+    
+    func bindViewsRx() {
+        viewModel.titleObservable
+            .bind(to: titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.temperatureObservable
+            .bind(to: temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.weatherIconObservable
+            .bind(to: weatherIcon.rx.image)
+            .disposed(by: disposeBag)
+        
+        viewModel.dataLoadingErrorObservable
+            .observeOn(MainScheduler.asyncInstance)
+            .subscribe(onNext: { [weak self] errorDescription in
+                let alert = UIAlertController(title: "Weather loading error", message: errorDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self?.present(alert, animated: true, completion: nil)
+            }).disposed(by: disposeBag)
+    }
+}
+
+extension LocationWeatherViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.collectionData.count
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.collectionData[section].rows.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return viewModel.collectionData[indexPath.section].rows[indexPath.row].dequeue(collectionView: collectionView)
+    }
+    
+    
 }
--- Weather-task/Screens/LocationWeather/viewModel/LocationWeatherViewModel.swift
+++ Weather-task/Screens/LocationWeather/viewModel/LocationWeatherViewModel.swift
@@ -7,7 +7,123 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxRelay
 
 class LocationWeatherViewModel {
+    private var location: LocationWeatherData
+    private let weatherProvider: WeatherAPIClient
+    private let weatherIconProvider: WeatherIconProvider
+    private let dataLoadingErrorRelay = BehaviorRelay<String?>(value: nil)
+    private lazy var temperatureRelay: BehaviorRelay<String?> = {
+        return BehaviorRelay(value: createTemperatureDescription())
+    }()
+    private lazy var titleRelay: BehaviorRelay<String?> = {
+        return BehaviorRelay(value: createTitle())
+    }()
+    private lazy var weatherIconRelay: BehaviorRelay<UIImage?> = {
+        return BehaviorRelay<UIImage?>(value: nil)
+    }()
     
+    lazy var dataLoadingErrorObservable: Observable<String> = {
+        return dataLoadingErrorRelay.skip(1).compactMap { $0 }.debounce(.seconds(5), scheduler: MainScheduler.asyncInstance)
+    }()
+    lazy var titleObservable: Observable<String> = {
+        return titleRelay.compactMap { $0 }
+    }()
+    lazy var temperatureObservable: Observable<String> = {
+        return temperatureRelay.compactMap { $0 }
+    }()
+    lazy var weatherIconObservable: Observable<UIImage> = {
+        return weatherIconRelay.compactMap { $0 }
+    }()
+    
+    var collectionData: [WeatherDataSection] = []
+    init(location: LocationWeatherData, weatherProvider: WeatherAPIClient, weatherIconProvider: WeatherIconProvider) {
+        self.location = location
+        self.weatherProvider = weatherProvider
+        self.weatherIconProvider = weatherIconProvider
+        loadWeatherIcon()
+        loadWeatherIfNeeded(for: location)
+        loadWeatherDetails()
+    }
+    
+    private func createTitle() -> String {
+        let cityName = location.cityName ?? ""
+        let weatherCondition = location.currentWeather?.weatherConditionDescription ?? ""
+        return "\(cityName)\n\(weatherCondition.capitalized)"
+    }
+    
+    private func createTemperatureDescription() -> String? {
+        if let temp = location.currentWeather?.temperature {
+            return "\(temp)°"
+        }
+        return nil
+    }
+    
+    private func loadWeatherIcon() {
+        if let iconName = location.currentWeather?.weatherConditionIconName {
+            _ = weatherIconProvider.setImage(for: iconName) { [weak self] data in
+                if let data = data {
+                    let image = UIImage(data: data)
+                    self?.weatherIconRelay.accept(image)
+                }
+            }
+        }
+    }
+    
+    private func loadWeatherIfNeeded(for location: LocationWeatherData) {
+        guard let forecastTimestamp = location.forecastTimestamp else {
+            loadWeather(for: location)
+            return
+        }
+        if (Date().timeIntervalSince1970 - 360) > forecastTimestamp {
+            loadWeather(for: location)
+        }
+    }
+    
+    private func loadWeather(for location: LocationWeatherData) {
+        weatherProvider.loadCurrentWeather(latitude: location.cityCoordinates.latitude, longitude: location.cityCoordinates.longitude) { [weak self] result in
+            switch result {
+            case .failure(_):
+                self?.dataLoadingErrorRelay.accept("Problem with loading forecast")
+            case .success(let currentWeather):
+                self?.location.currentWeather = currentWeather
+                self?.location.forecastTimestamp = Date().timeIntervalSince1970
+                self?.updateBasicData()
+            }
+        }
+    }
+    
+    private func loadWeatherDetails() {
+        
+    }
+    
+    private func updateBasicData() {
+        temperatureRelay.accept(createTemperatureDescription())
+        titleRelay.accept(createTitle())
+        loadWeatherIcon()
+    }
+}
+
+protocol WeatherDataCellViewModel {
+    func dequeue(collectionView: UICollectionView) -> UICollectionViewCell
+}
+
+enum WeatherDataSection {
+    case basicData(rows: [WeatherDataCellViewModel])
+    case hourlyForecast(rows: [WeatherDataCellViewModel])
+    case dailyForecast(rows: [WeatherDataCellViewModel])
+    
+    var rows: [WeatherDataCellViewModel] {
+        switch self {
+        case .basicData(let rows):
+            return rows
+        case .hourlyForecast(let rows):
+            return rows
+        case .dailyForecast(let rows):
+            return rows
+        }
+    }
 }
