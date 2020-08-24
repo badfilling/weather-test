437e98bf141a4c32be0b7c20f0ff9775ca0dbeb1
stepaniuk.artur <211650@edu.p.lodz.pl>
2020-08-24 19:51:17 +0200

    add forecast dto & model

---
+++ Weather-task/Models/DTOs/DetailForecastDTO.swift
@@ -0,0 +1,33 @@
+//
+//  DetailForecastDTO.swift
+//  Weather-task
+//
+//  Created by Artur Stepaniuk on 24/08/2020.
+//  Copyright © 2020 Artur Stepaniuk. All rights reserved.
+//
+
+import Foundation
+
+struct DetailForecastDTO: Decodable {
+    let list: [DetailForecastDataDTO]?
+}
+
+struct DetailForecastDataDTO: Decodable {
+    let main: CurrentWeatherMainDTO?
+    let weather: [WeatherConditionDTO]?
+    let forecastDate: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case main
+        case weather
+        case forecastDate = "dt_txt"
+    }
+}
+
+extension DetailForecastDataDTO {
+    func convert() throws -> WeatherForecast {
+        guard let temperature = main?.temperature,
+            let dateISO = forecastDate else { throw NetworkError.dataCorrupted }
+        return WeatherForecast(temperature: Int(temperature), dateISO: dateISO, weatherConditionIconName: weather?[0].icon)
+    }
+}
---
+++ Weather-task/Models/WeatherForecast.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherForecast.swift
+//  Weather-task
+//
+//  Created by Artur Stepaniuk on 24/08/2020.
+//  Copyright © 2020 Artur Stepaniuk. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherForecast {
+    let temperature: Int
+    let dateISO: String
+    let weatherConditionIconName: String?
+}
